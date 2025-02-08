72da93d9fe9a217b2bfd50c14aed7f6f74aaacec
tenok94 <tenok94@gmail.com>
2025-02-08 00:10:01 -0300

    arreglando error en turrnos 3

--- routes/turnosRoutes.js
+++ routes/turnosRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Turno = require("../models/Turno");
+const Turno = require("../models/turno");
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Crear un turno
